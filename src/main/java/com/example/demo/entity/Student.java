28675e80aa3b6aeb3750bde441bac0d8e1e06d34
Charlie Tang <1345308560@qq.com>
2023-03-22 17:34:07 +0800

    修改Student.java

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,13 +1,17 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "student")
 public class Student {
     @Id
+    @Column(name = "id")
     private Integer id;
-
+    @Column(name = "name")
     private String name;
 
     public Integer getId() {
