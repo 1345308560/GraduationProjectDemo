b09f5a83a410fbc91f7c1cd2be3ef1b22da005ac
1iyan <tan1iyan@163.com>
2023-03-23 12:02:09 +0800

    添加user类

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,29 +2,32 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.User;
 import com.example.demo.dao.UserRepository;
+import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(path="/demo")
-public class MainController {
+public class UserController {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private UserService userService;
 
     @PostMapping(path="/add")
     public @ResponseBody String addNewUser (@RequestParam String name
-            , @RequestParam String email) {
+            , @RequestParam String id) {
         User n = new User();
         n.setName(name);
-        n.setEmail(email);
+        n.setId(id);
         userRepository.save(n);
         return "Saved";
     }
 
     @GetMapping(path="/all")
     public @ResponseBody Iterable<User> getAllUsers() {
-        return userRepository.findAll();
+        return userService.findAll();
     }
 }
 
--- src/main/java/com/example/demo/dao/UserRepository.java
+++ src/main/java/com/example/demo/dao/UserRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.dao;
 
+import com.example.demo.entity.Admin;
 import com.example.demo.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
     @Override
+    @Query(value="select * from user" ,nativeQuery=true)
     Iterable<User> findAll();
 }
